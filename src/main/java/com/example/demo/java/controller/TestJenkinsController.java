8fc9ab6489c93a2d09c51be074f96295d22b7e13
Tiger <963076844@qq.com>
2019-03-24 16:16:21 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/java/controller/TestJenkinsController.java
@@ -0,0 +1,18 @@
+package com.example.demo.java.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * Created by Aaron on 2019/3/24.
+ */
+@RestController
+public class TestJenkinsController {
+
+    @RequestMapping("/test")
+   public String sayHello(){
+       return "hello:jenkins"+new Date();
+   }
+}
